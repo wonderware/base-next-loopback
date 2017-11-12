bd1ebda7a35a61627db49dbd8edb86fb21be3eb9
Patrick Walters <wonderwarestudios@gmail.com>
2017-11-11 20:15:18 -0500

    fix lint errors and startup script

--- pages/Home.js
+++ pages/Home.js
@@ -3,13 +3,14 @@ import React from "react";
 export default function Home() {
 
 	return <div className="jumbotron jumbotron-fluid">
-			<div className="container">
-				<h1 className="display-3">
-					Hello there world.
-				</h1>
-				<p className="lead">
-					It sure is nice to see you there.
-				</p>
-			</div>
+		<div className="container">
+			<h1 className="display-3">
+				Hello there world.
+			</h1>
+
+			<p className="lead">
+				It sure is nice to see you there.
+			</p>
 		</div>
+	</div>
 }
